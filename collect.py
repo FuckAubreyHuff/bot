47145460240de1ef32ec5fb886c404ddd3e680e9
FuckAubreyHuff <77758640+FuckAubreyHuff@users.noreply.github.com>
2021-01-21 13:26:29 -0600

    Add files via upload

--- collect.py
+++ collect.py
@@ -4,10 +4,10 @@ import random
 
 print("Bot Starting...")
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_SECRET = ''
+CONSUMER_KEY = 'bfcVqgGo80zIXyR1Kbcg0ggAi'
+CONSUMER_SECRET = 'P48ZPHiWMhaKXAkxAR8S9jiztAF6qAa3QynrA9PdTTmEgrxn4H'
+ACCESS_TOKEN = '1176869078738378753-wY1noHWcRp4MGRDFuMSEvFFJxLXTId'
+ACCESS_SECRET = 'EpiPeDFrRB2XzG4jgGt6tWp4p05ww4XwFUdXEQco4FPcS'
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -19,10 +19,13 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 # Define screen name (Twitter @) to use
 screen_name = 'aubrey_huff'
 
+# Number of followers to scrape
+f_count = 10
+
 # Function to save followers in a text file
 def getting_followers(file):
     cursor = tweepy.Cursor(api.followers, screen_name)
-    for user in cursor.items(500):
+    for user in cursor.items(f_count):
         file.write(str(user.screen_name)+ "\n")
         # Check if it's gathering data correctly
         #print("follower:" + user.screen_name)
